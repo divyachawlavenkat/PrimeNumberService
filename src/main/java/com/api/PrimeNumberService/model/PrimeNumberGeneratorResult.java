6833b0528915b7c06cea61ec1b75325e4f409751
divyavenkatesh <divya22295@gmail.com>
2023-03-18 05:25:13 +0000

    Prime Number Generator Response with initial and primes

---
+++ src/main/java/com/api/PrimeNumberService/model/PrimeNumberGeneratorResult.java
@@ -0,0 +1,19 @@
+package com.api.PrimeNumberService.model;
+
+import lombok.Data;
+
+import java.util.*;
+/**
+ *  prime number generator result
+ *  @author divyavenkatesh
+ */
+@Data
+public class PrimeNumberGeneratorResult {
+    private int initial;
+    private List<Integer> primes;
+
+    public PrimeNumberGeneratorResult(int initial, List<Integer> primes) {
+        this.initial = initial;
+        this.primes = primes;
+    }
+}
